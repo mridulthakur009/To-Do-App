2c7169cb553b34c40170b8b6d7f7ada5d452f301
mridulthakur009 <mridul.thakur.007@gmail.com>
2023-11-10 18:02:02 +0530

    final

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-function val(num){
-    document.getElementById("inputBox").value += num;
+
+function add() {
+    let input = document.getElementById("input");
+
+    if (input.value == "") {
+        alert("Please enter any task");
+    }
+    else{
+    document.querySelector(".cardss").innerHTML +=    
+    `
+    <div class="card m-3">
+      <div class="card-body">
+       <input type="checkbox" class="checkbox">
+        <p class="text"> &nbsp;
+        ${document.querySelector("#input").value}
+        </p>
+        <label for="checkbox"></label>
+        <button class="delete"><i class="fa-solid fa-xmark" style="color: #ff0000;" onclick="removeTask(this.parentNode)" ></i></button>
+      </div>
+    </div>
+    `;
+    input.value = "";
+}
 }
 
-function clearRec(){
-    document.getElementById("inputBox").value = "";
+function removeTask(task) {
+    task.parentNode.remove();
 }
 
-function calc() {
-    let value = document.getElementById("inputBox").value;
-    document.getElementById("inputBox").value = eval(value)
-}
