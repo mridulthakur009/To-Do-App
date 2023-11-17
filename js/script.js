d5ec614f00cdc3e573fa1f720f6c849434405e7e
mridulthakur009 <mridul.thakur.007@gmail.com>
2023-11-17 18:03:32 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 function add() {
-  let input = document.getElementById("input");
-
-  if (input.value == "" || input.value == null) {
+    console.time(" timer");
+    let input = document.getElementById("input");
+    
+    if (input.value == "" || input.value == null || input.value == " ") {
      document.getElementById("inputValidation").innerHTML = " * Enter Task ! ! !" 
      
-  } else {
-    
+    } else {
+        
       let data = document.querySelector(".cardss");
-
+      
       let newTask = document.createElement("div");
       newTask.classList.add("card");
 
@@ -16,15 +17,16 @@ function add() {
               <input type="checkbox" class="checkbox">
               <p class="text"> ${input.value}</p>
               <label for="checkbox"></label>
-              <button class="delete">
-                  <i class="fa-solid fa-xmark" style="color: #ff0000;" onclick="removeTask(this.parentNode.parentNode)"></i>
+              <button class="delete" onclick="removeTask(this.parentNode.parentNode)">
+                  <i class="fa-solid fa-xmark" style="color: #ff0000;" ></i>
               </button>
           </div>
-      `;
-
+          `;
+          
       data.insertBefore(newTask, data.firstChild);
       input.value = "";
   }
+  console.timeEnd(" timer");
 }
 
 function removeTask(task) {
